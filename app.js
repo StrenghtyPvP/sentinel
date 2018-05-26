7b03a24a51f3fab08b8d798a752406ba1c45403c
StrenghtyPvP <32174283+StrenghtyPvP@users.noreply.github.com>
2018-05-26 17:51:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-const config = require("./config.json");
+const config = require("sentinel/config.json");
 
 client.on("ready", () => {
 
